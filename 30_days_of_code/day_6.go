4442d5f56e9630c2a4adb5d703683363f60763db
Mahendra Kathirvel <astromahi@gmail.com>
2016-01-07 21:57:33 +0530

    Updated day 6 challenge

--- 30_days_of_code/day_6.go
+++ 30_days_of_code/day_6.go
@@ -7,7 +7,11 @@ func main() {
 
 	var N int
 
-	fmt.Scanf("%d", &N)
+	_, err := fmt.Scanf("%d", &N)
+	if err != nil {
+		fmt.Println("something wrong")
+		return
+	}
 
 	if N < 1 || N > 100 {
 		fmt.Println("N constrains failed.")
@@ -17,11 +21,11 @@ func main() {
 	for i := 1; i <= N; i++ {
 		for j := 1; j <= N; j++ {
 			if (i + j) > N {
-				fmt.Printf("#")
+				fmt.Print("#")
 			} else {
 				fmt.Print(" ")
 			}
 		}
-		fmt.Printf("\n")
+		fmt.Print("\n")
 	}
 }
