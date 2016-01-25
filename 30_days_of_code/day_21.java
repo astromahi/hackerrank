6518cbd93c6e68ffa96e65a34aef31efe4a7b0dc
Mahendran Kathirvel <astromahi@gmail.com>
2016-01-25 16:44:10 +0530

    Added day 21 challenge

---
+++ 30_days_of_code/day_21.java
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-21-generics
+// Day 21: Generics!
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+    public <T> void printArray(T[] array) {
+        for (T element: array) {
+            System.out.println(element);
+        }
+    }
+}
+
+public class Solution
+{
+    
+
+    public static void main( String args[] )
+    {
+        Printer myPrinter=new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello","World"};
+        myPrinter.printArray( intArray  );
+        myPrinter.printArray( stringArray );
+        int count=0;
+		for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+            if(name.equals("printArray"))
+              count++;
+        }
+        
+        if(count>1)System.out.println("Method overloading is not allowed!");
+        assert count==1;
+
+    } 
+}
