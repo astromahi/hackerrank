d53e93ad62fc25a3b112624b0400c63dfc1b9173
Mahendran Kathirvel <astromahi@gmail.com>
2016-01-29 00:58:48 +0530

    Added day 27 challenge

---
+++ 30_days_of_code/day_27.go
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-27-testing-part-2
+// Day 27: Testing Part II!
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// NO OF TESTCASES
+	fmt.Println("5")
+
+	// YES
+	fmt.Println("5 3")
+	fmt.Println("0 -8 2 5 10")
+
+	// NO
+	fmt.Println("6 4")
+	fmt.Println("-12 0 -11 -1 -6 7")
+
+	// YES
+	fmt.Println("7 6")
+	fmt.Println("-100 -70 0 -50 0 3 6")
+
+	// NO
+	fmt.Println("10 8")
+	fmt.Println("-20 -17 -13 120 -9 -122 -16 -111 -19 0")
+
+	// YES
+	fmt.Println("3 3")
+	fmt.Println("0 -500 15")
+}
