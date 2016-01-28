2f351f53eed6deb5a5a857906f035722b46391e3
Mahendran Kathirvel <astromahi@gmail.com>
2016-01-29 00:59:38 +0530

    Added day 28 challenge

---
+++ 30_days_of_code/day_28.java
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-28-regex-patterns-intro-to-databases
+// Day 28: RegEx, Patterns, and Intro to Databases!
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      while(testCases>0){
+         String pattern = in.nextLine();
+             //Write your code
+          try {
+              Pattern p = Pattern.compile(pattern);
+              if (p != null) {
+                  System.out.println("Valid");
+              }
+          } catch (PatternSyntaxException ex) {
+              System.out.println("Invalid");
+          }
+          
+      }
+   }
+}
