0085c8e149c1ead0588b7b880b21eb3955fce47f
Mahendran Kathirvel <astromahi@gmail.com>
2016-01-30 02:37:02 +0530

    Added day 23 challenge

---
+++ 30_days_of_code/day_23.java
@@ -0,0 +1,83 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-23-review-and-binary-trees
+// Day 23: Review+Binary Trees!
+
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution {
+	static void levelOrder(Node root){
+      //Write your code here
+    int h = height(root);
+    int i;
+    for (i=1; i<=h; i++) {
+        printLevel(root, i);
+    }
+      
+    }
+ 
+    static int height(Node root)
+    {
+        if (root == null)
+           return 0;
+        else
+        {
+            
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+             
+          
+            if (lheight > rheight)
+                return(lheight+1);
+            else return(rheight+1); 
+        }
+    }
+ 
+    
+    static void printLevel (Node root ,int level)
+    {
+        if (root == null)
+            return;
+        if (level == 1)
+            System.out.print(root.data + " ");
+        else if (level > 1)
+        {
+            printLevel(root.left, level-1);
+            printLevel(root.right, level-1);
+        }
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
