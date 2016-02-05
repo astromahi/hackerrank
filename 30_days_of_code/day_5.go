906a0d0e37ef801d70e0388f2cd62ec589f53100
Mahendran Kathirvel <astromahi@gmail.com>
2016-02-05 14:52:06 +0530

    Updated day 5 challenge

--- 30_days_of_code/day_5.go
+++ 30_days_of_code/day_5.go
@@ -1,3 +1,6 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-5-loops
+// Day 5: Loops!
+
 package main
 
 import (
@@ -5,53 +8,18 @@ import (
 	"math"
 )
 
-type testCase struct {
-	a, b, N int
-}
-
 func main() {
 
-	var T, seq int
-	var tc testCase
-	var testCases []testCase
+	var t, a, b, n, seq int
 
-	fmt.Scanf("%d\n", &T)
-	if T < 0 || T > 500 {
-		fmt.Println("testcase constraints failed.")
-		return
-	}
+	fmt.Scanf("%d", &t)
 
-	for i := 0; i < T; i++ {
-
-		fmt.Scanf("%d%d%d\n", &tc.a, &tc.b, &tc.N)
-
-		if tc.a < 0 || tc.a > 50 {
-			fmt.Println("a constrains failed.")
-			return
-		}
-
-		if tc.b < 0 || tc.b > 50 {
-			fmt.Println("b constraints failed.")
-			return
-		}
-
-		if tc.N < 1 || tc.N > 15 {
-			fmt.Println("N constraints failed.")
-			return
-		}
-
-		testCases = append(testCases, tc)
-
-	}
-
-	for _, c := range testCases {
-		for j := 0; j < c.N; j++ {
-			if j == 0 {
-				seq = c.a + (int(math.Pow(2, float64(j))) * c.b)
-			} else {
-				seq = seq + (int(math.Pow(2, float64(j))) * c.b)
-			}
-
+	for i := 0; i < t; i++ {
+		fmt.Scanf("%d%d%d", &a, &b, &n)
+		seq = a + b
+		fmt.Print(seq, " ")
+		for j := 1; j < n; j++ {
+			seq = seq + int(math.Pow(2, float64(j)))*b
 			fmt.Printf("%d ", seq)
 		}
 		fmt.Print("\n")
