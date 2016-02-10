307c976e026aedadee1b7144ae36fa4f7db1b7bb
Mahendran Kathirvel <astromahi@gmail.com>
2016-02-10 20:46:59 +0530

    Added day 10 challenge alternate solution.

---
+++ 30_days_of_code/day_10_1.go
@@ -0,0 +1,34 @@
+// https://www.hackerrank.com/contests/30-days-of-code/challenges/day-10-binary-numbers
+// Day 10: Binary Numbers!
+
+package main
+
+import (
+	"fmt"
+)
+
+var stack []int
+
+func toBinary(n int) {
+	for n >= 1 {
+		rem := n % 2
+		stack = append(stack, rem)
+		n = n / 2
+	}
+
+}
+
+func main() {
+	var t, n int
+	fmt.Scanf("%d\n", &t)
+
+	for i := 0; i < t; i++ {
+		fmt.Scanf("%d\n", &n)
+		toBinary(n)
+		for i := 1; i <= len(stack); i++ {
+			fmt.Print(stack[len(stack)-i])
+		}
+		fmt.Print("\n")
+		stack = nil
+	}
+}
