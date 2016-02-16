4e520b3169017113d59b8d32687e8c31b2c3a31d
Mahendran Kathirvel <astromahi@gmail.com>
2016-02-16 19:38:14 +0530

    Added regex introduction 3rd solution.

---
+++ regex/introduction/mdndc.go
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/matching-digits-non-digit-character
+// Matching Digits & Non-Digit Characters
+
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"bufio"
+	"os"
+)
+
+func main() {
+	var input string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input = scanner.Text()
+	}
+
+	re := regexp.MustCompile(`\d\d\D\d\d\D\d\d\d\d`)
+	matched := re.MatchString(input)
+
+	fmt.Println(matched)
+}
