a912cb82eddf055c66082af421fea48628c83249
Mahendran Kathirvel <astromahi@gmail.com>
2016-08-09 19:32:29 +0530

    Formatted the solution

--- pattern_printing/hack_10.go
+++ pattern_printing/hack_10.go
@@ -8,12 +8,12 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n-i-1; j++ {
+	for i:=0; i<n; i++ {
+		for j:=0; j<n-i-1; j++ {
 			fmt.Print(" ")
 		}
 
-		for k := 0; k <= i; k++ {
+		for k:=0; k<=i; k++ {
 			fmt.Print("#")
 		}
 
